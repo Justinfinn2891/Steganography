63bdc45d515fe08575f923aa65e41fb673eeca62
Gabriel Speer <gspeer2@ut.utm.edu>
2023-09-19 17:56:30 -0500

    yes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-  
-  
+  cout<<"What type of operation are you running?(E or D/)"<<endl;
+  char operation;
+  cin>>operation;
+  operation=tolower(operation);
+  if(operation=='c'){
+    cout<<"success"<<endl;
+  }
+  else if(operation=='d'){
+    cout<<"success"<<endl;
+  }
+  else{
+    cout<<"Error"<<endl;
+  }
   return 0;
 }
