c66c390ddbc4f413c5e97da62ee7bd9e94e8340b
Justinfinn2891 <jfinn5@ut.utm.edu>
2023-09-19 18:10:03 -0500

    .cpp file

--- Steganography.cpp
+++ Steganography.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <vector>
 #include "Steganography.h"
-
+#include <fstream>
 
 using namespace std;
 
 
 void Graphy::readImage(string fileName){
+  ifstream file;
 
+  file.open(fileName);
+
+  file >> magicNumber >> width >> height >> maxColor;
+
+  int i = 0;
+  colorData.push_back(i);
+  
+  while(file)
+    {
+      i++;
+      colorData.push_back(i);
+      
+    }
 }
 void Graphy::printImage(string fileName)
 {
