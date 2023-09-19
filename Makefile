75c0d22e6b86a2b66207d57aa798c936e94516c0
Justinfinn2891 <jfinn5@ut.utm.edu>
2023-09-19 17:42:27 -0500

    makefile and steg

---
+++ Makefile
@@ -0,0 +1,15 @@
+CC = g++
+CFLAGS = -g -Wall -Wextra
+
+default: steganography
+
+steganography: main.o Steganography.o
+	$(CC) $(CFLAGS) -o steganography main.o Steganography.o
+
+Steganography.o: Steganography.cpp Steganography.h
+	$(CC) $(CFLAGS) -c Steganography.cpp
+
+main.o: main.cpp Steganography.h
+	$(CC) $(CFLAGS) -c main.cpp
+
+clean: $(RM) steganography *.o *~ 
---
+++ Steganography.h
@@ -0,0 +1,30 @@
+#ifndef STEGANOGRAPHY_H
+#define STEGANOGRAPHY_H
+
+#include <string>
+
+class Graphy
+{
+ private:
+  std::string magicNumber;
+  int width;
+  int height;
+  int maxColor;
+  vector<int> colorData;
+  std::string cipherText;
+  int getNthBit(char cipherChar, int n);
+  
+    
+ public:
+  void readImage(std::string fileName);
+  void printImage(std::string fileName);
+  void readCipherText(std::string fileName);
+  void printCipherText(std::string fileName);
+  void cleanImage();
+  void encipher();
+  void decipher();
+  
+  
+};
+
+#endif // STEGANOGRAPHY_H
