5a60ad1b57c2d1434f884ba6b7a9621c443f1fb9
Gabriel Speer <gspeer2@ut.utm.edu>
2023-09-19 18:19:21 -0500

    Start encode

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 
 #include <iostream>
 #include <vector>
+#include "Steganography.h"
 
 using namespace std;
 
 int main() {
+  string PPM;
+  string input;
+  string code;
+  Graphy Stegano;
+
+  
   cout<<"What type of operation are you running?(E or D/)"<<endl;
   char operation;
   cin>>operation;
   operation=tolower(operation);
+  
   if(operation=='e'){
-    cout<<"success"<<endl;
+    cout<<"What is the PPM filename?"<<endl;
+    cin>>PPM;
+    Stegano.readImage(PPM);
+    cout<<"What is the name of the input text file?"<<endl;
+    cin>>input;
+    Stegano.readCipherText(input);
+    cout<<"What should be the name of the Encoded PPM file?"<<endl;
+    cin>>code;
+    
   }
   else if(operation=='d'){
     cout<<"success"<<endl;
